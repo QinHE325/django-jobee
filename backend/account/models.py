2e523f283a94d13e90cbea8484252647ae878762
Qin He <heqin@usc.edu>
2022-06-20 00:34:21 -0400

    User Auth

---
+++ backend/account/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
+    resume = models.FileField(null=True)
+    
